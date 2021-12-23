bbe299493f30f7a1d298b35a0ffdd3d4beccb8e1
Ed Clement <ed@devintent.com>
2021-12-23 12:57:53 -0500

    build(typescript): remove babel from the build process and rely only on TypeScript for transpiling files
    fix #2

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   name: 'verdaccio-prometheus-middleware',
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/types/*'],
+  moduleFileExtensions: ['js', 'ts'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   reporters: ['default', ['jest-junit', { outputDirectory: './coverage/jest', outputName: 'results.xml' }]],
 };
